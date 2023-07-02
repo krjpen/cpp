787dc26121bcd4584724b32e9106032029ae7f7f
Harry Jo <126487712+krjpen@users.noreply.github.com>
2023-07-02 15:02:05 +0900

    Create EX9.cpp

---
+++ EX9.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int x, a, b;
+  cin >> x >> a >> b;
+
+  // 1.の出力
+  x++;
+  cout << x << endl;
+
+  // ここにプログラムを追記
+  
+  int c = x*(a+b);
+  int d = c*c;
+  cout << c << endl;
+  cout << d << endl;
+  cout << d-1 << endl;
+}
